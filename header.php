b0e69691618ac86656dab0948c6800690c6ec1d4
Dimas nugroho <144247337+dimas1625@users.noreply.github.com>
2024-12-06 08:19:17 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,46 @@
+
+<header class="app-header">
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <ul class="navbar-nav">
+            <li class="nav-item d-block d-xl-none">
+              <a class="nav-link sidebartoggler nav-icon-hover" id="headerCollapse" href="javascript:void(0)">
+                <i class="ti ti-menu-2"></i>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link nav-icon-hover" href="./notifikasi.php">
+                <i class="ti ti-bell-ringing"></i>
+                <div class="notification bg-primary rounded-circle"></div>
+              </a>
+            </li>
+          </ul>
+          <div class="navbar-collapse justify-content-end px-0" id="navbarNav">
+            <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-end">
+              <li class="nav-item dropdown">
+              <a class="nav-link" href="javascript:void(0)" id="drop2" data-bs-toggle="dropdown"
+                  aria-expanded="false">
+                  <img src="../../../assets/images/profile/dimas.jpg" alt="" width="40" height="40"
+                    class="rounded-circle" />
+                    <p style="margin-top: 15px; margin-left: 10px;">2341720033 / Muhammad Dimas Ajie Nugroho</p>
+                </a>
+                <div class="dropdown-menu dropdown-menu-end dropdown-menu-animate-up" aria-labelledby="drop2">
+                  <div class="message-body">
+                    <a href="./profile.php" class="d-flex align-items-center gap-2 dropdown-item">
+                      <i class="ti ti-user fs-6"></i>
+                      <p class="mb-0 fs-3">Profil</p>
+                    </a>                  
+                    <button id="logoutButton" class="btn btn-outline-primary mx-3 mt-2 d-block">Keluar</button>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <script src="../../../assets/libs/jquery/dist/jquery.min.js"></script>
+  <script src="../../../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="../../../assets/js/sidebarmenu.js"></script>
+  <script src="../../../assets/js/app.min.js"></script>
+  <script src="../../../assets/libs/apexcharts/dist/apexcharts.min.js"></script>
+  <script src="../../../assets/libs/simplebar/dist/simplebar.js"></script>
+  <script src="../../../assets/js/dashboard.js"></script>
---
+++ sidebar.php
@@ -0,0 +1,72 @@
+
+<aside class="left-sidebar">
+      <!-- Sidebar scroll-->
+      <div>
+        <div class="brand-logo d-flex justify-content-center align-items-center">
+          <a href="./index.html" class="text-nowrap logo-img">
+            <img src="../../../assets/images/logos/Sibeta1.png" width="150"  alt="" />
+          </a>
+          <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
+            <i class="ti ti-x fs-8"></i>
+          </div>
+        </div>
+        <!-- Sidebar navigation-->
+        <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
+          <ul id="sidebarnav">
+            <li class="nav-small-cap">
+              <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+              <span class="hide-menu">Beranda</span>
+            </li>
+            <li class="sidebar-item">
+              <a class="sidebar-link" href="dashboard.php" aria-expanded="false">
+                <span>
+                  <i class="ti ti-layout-dashboard"></i>
+                </span>
+                <span class="hide-menu">Dashboard</span>
+              </a>
+            </li>
+            <li class="sidebar-item">
+              <a class="sidebar-link" href="./unggah-dokumenPusat.php" aria-expanded="false">
+                <span>
+                  <i class="ti ti-file-text"></i>
+                </span>
+                <span class="hide-menu">Unggah Dokumen Pusat</span>
+              </a>
+            </li>
+            <li class="sidebar-item">
+              <a class="sidebar-link" href="./unggah-dokumenJurusan.php" aria-expanded="false">
+                <span>
+                  <i class="ti ti-file-text"></i>
+                </span>
+                <span class="hide-menu">Unggah Dokumen Jurusan</span>
+              </a>
+            </li>
+            <li class="sidebar-item">
+              <a class="sidebar-link" href="./unggah-tugasAkhir.php" aria-expanded="false">
+                <span>
+                  <i class="ti ti-file-text"></i>
+                </span>
+                <span class="hide-menu">Unggah Tugas Akhir</span>
+              </a>
+            </li>
+            <li class="sidebar-item">
+              <a class="sidebar-link" href="./status-verifikasi.php" aria-expanded="false">
+                <span>
+                  <i class="ti ti-checklist"></i>
+                </span>
+                <span class="hide-menu">Status Verifikasi</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <!-- End Sidebar navigation -->
+      </div>
+      <!-- End Sidebar scroll-->
+    </aside>
+    <script src="../../../assets/libs/jquery/dist/jquery.min.js"></script>
+  <script src="../../../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="../../../assets/js/sidebarmenu.js"></script>
+  <script src="../../../assets/js/app.min.js"></script>
+  <script src="../../../assets/libs/apexcharts/dist/apexcharts.min.js"></script>
+  <script src="../../../assets/libs/simplebar/dist/simplebar.js"></script>
+  <script src="../../../assets/js/dashboard.js"></script>
